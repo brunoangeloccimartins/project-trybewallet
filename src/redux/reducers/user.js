bcb4edc34a2fd29bc5cfe924038d43f116092cdd
brunoangeloccimartins <brunoangelocci@gmail.com>
2023-05-12 17:55:40 -0300

    Store quase finalizada

--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -1,1 +1,11 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+const USER_STATE = {}
+
+const user = (state = USER_STATE, action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
+
+export default user
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,2 +1,6 @@
+import { composeWithDevTools } from "@redux-devtools/extension/lib/types/logOnly"
+import { applyMiddleware, legacy_createStore as createStore } from "redux"
+import thunk from "redux-thunk"
+
 // configure aqui sua store
-.
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)))
